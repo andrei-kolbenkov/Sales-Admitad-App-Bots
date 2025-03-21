7d3ce20e1cd152ed6a1103332ae703a21ce74eed
Andrei <151627787+andrei-kolbenkov@users.noreply.github.com>
2025-03-21 16:08:44 +0300

    Update bot_parse.py

--- telegram-bot/bot_parse.py
+++ telegram-bot/bot_parse.py
@@ -1,62 +1,95 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from aiogram.utils import executor
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.interval import IntervalTrigger
-
 from message import get_coupons, get_advcampaigns_for_website2
 
 
-
-
-
-scheduler = AsyncIOScheduler()
-
 ADMIN_CHAT_ID = []
 API_TOKEN = ''
-
 bot = Bot(token=API_TOKEN)
 
-
+# Глобальный планировщик
+scheduler = AsyncIOScheduler()
 
 async def periodic_loading():
     print('Начало парсинга')
-    # await bot.send_message(, 'Парсинг начался')
+    try:
+        for admin in ADMIN_CHAT_ID:
+            await bot.send_message(admin, 'Парсинг начался')
+    except Exception as e:
+        print(e)
+        await asyncio.sleep(30)
+        for admin in ADMIN_CHAT_ID:
+            await bot.send_message(admin, 'Парсинг начался')
 
     try:
         messages_for_delete, text, go_out_shops = get_coupons()
-        # await bot.send_message(, text)
-        # await bot.send_message(, text=f'Магазины, которые больше не с нами: {go_out_shops}')
-        # await bot.send_message(, text=f'Магазины, которые больше не с нами: {go_out_shops}')
+        try:
+            for admin in ADMIN_CHAT_ID:
+                await bot.send_message(admin, text)
+                await bot.send_message(admin, text=f'Магазины, которые больше не с нами: {go_out_shops}')
+        except Exception as e:
+            print(e)
+            await asyncio.sleep(30)
+            for admin in ADMIN_CHAT_ID:
+                await bot.send_message(admin, text)
+                await bot.send_message(admin, text=f'Магазины, которые больше не с нами: {go_out_shops}')
         for message in messages_for_delete.values():
             if (message[0] is not None) and (message[1] is not None):
-                await bot.delete_message(message[0], message[1])
+                try:
+                    await bot.delete_message(message[0], message[1])
+                except Exception as e:
+                    print(e)
+                    await asyncio.sleep(30)
+                    await bot.delete_message(message[0], message[1])
     except Exception as e:
         print('Нет сообщений для удаления:', e)
 
+    
     try:
         products_for_delete, not_rub = get_advcampaigns_for_website2()
-        for product in products_for_delete:
-            await bot.delete_message(product[0], product[1])
+        try:
+            for product in products_for_delete:
+                try:
+                    await bot.delete_message(product[0], product[1])
+                except Exception as e:
+                    print(e)
+                    await asyncio.sleep(30)
+                    await bot.delete_message(product[0], product[1])
+        except Exception as e:
+            print('Нет постов с товарами для удаления:', e)
         print(not_rub)
-        # await bot.send_message(, f'Товары не доступны: {not_rub}')
-        # await bot.send_message(, f'Товары не доступны: {not_rub}')
     except Exception as e:
-        print('Нет постов с товарами для удаления:', e)
+        print(e)
 
     print('Парсинг завершен')
-    await bot.send_message(, 'Парсинг завершен')
+    try:
+        for admin in ADMIN_CHAT_ID:
+            await bot.send_message(admin, 'Парсинг завершен')
+    except Exception as e:
+        print(e)
+        await asyncio.sleep(30)
+        for admin in ADMIN_CHAT_ID:
+            await bot.send_message(admin, 'Парсинг завершен')
+
 
 
 if __name__ == '__main__':
-    # asyncio.run(periodic_loading())
+
     loop = asyncio.get_event_loop()
+    scheduler = AsyncIOScheduler(event_loop=loop)  # Используем текущий цикл событий
+
+    # Добавляем задачу для периодической загрузки
     scheduler.add_job(periodic_loading, IntervalTrigger(hours=4))
     scheduler.start()
-    loop.run_forever()
+
+    # Ручной запуск первого выполнения задачи
+    loop.create_task(periodic_loading())
+
+    try:
+        print("Запуск событийного цикла")
+        loop.run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        print("Остановка событийного цикла")
+        scheduler.shutdown()
